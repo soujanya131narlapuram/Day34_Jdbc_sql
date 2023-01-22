075ca006ed18b0bd635f45cc43200b7b7179976c
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-22 22:30:48 +0530

    'UC2 Commit'

--- src/main/java/com/bridgelabz/Jdbc_Main.java
+++ src/main/java/com/bridgelabz/Jdbc_Main.java
@@ -18,16 +18,25 @@ public class Jdbc_Main {
             // Execute a query
             ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
 
-            // Process the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("salary"));
-            }
+            displayResultSet(rs);
 
             // Close the connection
             conn.close();
 
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
+        }
+    }
+    public static void displayResultSet(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs.getString(i);
+                System.out.print(rsmd.getColumnName(i) + " " + columnValue);
+            }
+            System.out.println("");
         }
     }
 
